7bade5d0cda05c75cded32aa38e1d512de3668d5
Decagon <maximmajor4us@gmail.com>
2021-10-31 02:25:45 +0100

    get-delete-route

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -9,7 +9,7 @@ const bcrypt_1 = __importDefault(require("bcrypt"));
 const user_1 = __importDefault(require("../models/user"));
 //UPDATE USER
 router.put('/:id', async (req, res, next) => {
-    if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 const salt = await bcrypt_1.default.genSalt(10);
@@ -31,6 +31,30 @@ router.put('/:id', async (req, res, next) => {
     }
     else {
         return res.status(403).json("you can update only your account");
+    }
+});
+router.delete('/:id', async (req, res, next) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            await user_1.default.findByIdAndDelete(req.params.id);
+            return res.status(200).json("Account has been deleted");
+        }
+        catch (err) {
+            return res.status(500).json(err);
+        }
+    }
+    else {
+        return res.status(403).json("you can delete only your account");
+    }
+});
+//GET USER
+router.get('/:id', async (req, res, next) => {
+    try {
+        const user = await user_1.default.findById(req.params.id);
+        res.status(200).json(user);
+    }
+    catch (err) {
+        res.status(500).json(err);
     }
 });
 exports.default = router;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -8,7 +8,7 @@ import User from "../models/user"
 
 //UPDATE USER
 router.put('/:id', async (req:any, res:Response, next:NextFunction) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin ) {
+  if (req.body.userId === req.params.id || req.body.isAdmin ) {
       if (req.body.password){
         try {
           const salt = await bcrypt.genSalt(10)
@@ -32,6 +32,32 @@ router.put('/:id', async (req:any, res:Response, next:NextFunction) => {
         return res.status(403).json("you can update only your account")
       }
   
+});
+
+router.delete('/:id', async (req:any, res:Response, next:NextFunction) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin ) {
+        try {
+          await User.findByIdAndDelete(req.params.id)
+          return res.status(200).json("Account has been deleted")
+        } catch (err){
+          return res.status(500).json(err)
+        }
+      }
+      else {
+        return res.status(403).json("you can delete only your account")
+      }
+    
+});
+
+//GET USER
+router.get('/:id', async (req:any, res:Response, next:NextFunction) => {
+  try {
+    const user = await User.findById(req.params.id)
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+    
 });
 
 export default router;
