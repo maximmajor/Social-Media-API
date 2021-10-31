421d142cbeb601831a8676582ab744590c7e88df
Decagon <maximmajor4us@gmail.com>
2021-10-31 01:35:51 +0100

    update-user

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     //   useCreateIndex:true
       
     });
